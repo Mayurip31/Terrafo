8a10767be9a2f95c0a6599736b39974d697bb76f
Mayurip31 <mayuripawar6196@gmail.com>
2024-03-12 15:29:25 +0530

    ec2 module 1

--- EC2/main.tf
+++ EC2/main.tf
@@ -5,15 +5,20 @@ provider "aws" {
   
 }
 
+terraform {
+  backend "s3" {
+     bucket = "terraformconfigdata"
+     key    = "terraform.tfstate"
+     region = "us-east-1"
+     dynamodb_table = "terraform-staging"
+   }
+ }
+
 resource "aws_instance" "server" {
 
     ami = var.ami
     instance_type = var.instance_type
     key_name = var.key_name
-
     tags = var.tags
 
-
-
-  
 }
